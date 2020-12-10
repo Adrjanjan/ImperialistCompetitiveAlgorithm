78fdb27ce98f8bf489501f4d55c57cb8047bd756
Adrian Janakiewicz <adrjanjan@gmail.com>
2020-12-10 22:39:32 +0100

    #9 Refactor gridsearch

--- algorithm_evaluation/F1/F1_elliptic.py
+++ algorithm_evaluation/F1/F1_elliptic.py
@@ -1,11 +1,6 @@
-import matplotlib.pyplot as plt
 import tensorflow as tf
-from sklearn.model_selection import ParameterGrid
-
-from ica.ica import ICA
+from algorithm_evaluation.evaluation_helper import *
 from test_functions import CostFunction
-
-tf.config.run_functions_eagerly(True)
 
 
 class F1_Elliptic(CostFunction):
@@ -19,67 +14,12 @@ class F1_Elliptic(CostFunction):
         return self.elliptic_func(z)
 
 
-def create_and_save_plots_to_file(iterations_results, file_path):
-    # TODO
-    # min_vals = []
-    # for index_params in iterations_results:
-    #     index, params = index_params
-    #     min_vals.append(params["lowest_cost_per_iteration"])
-    # str(params["max_iterations"]),
+tf.config.run_functions_eagerly(True)
 
-    # x = metadata[0]
-    # y = metadata[1]
-    # plt.scatter(x, y, marker='v', color='r')
-    # plt.title('Scatter Plot Example')
-    # plt.show()
-    # plt.savefig(file_path + 'foo.png', bbox_inches='tight')
-    pass
-
-
-def create_and_save_params_grid_as_latex_table(iterations_results, file_path):
-    sep = " & "
-    endline = r"\\\hline" + "\n"
-
-    begin = r"\begin{tabular}{|c|c|c|c|c|c|c|}" + "\n" \
-            + sep.join(["Nr. wykresu",
-                        "Czas ewaluacji",
-                        "Wartość osiągniętego minimum",
-                        "Ostatnia iteracja",
-                        "Maksymalna liczba iteracji",
-                        "Liczba imperiów",
-                        "Liczba kolonii",
-                        r"Bezpośrednia asymilacja - $\beta$",
-                        "Współczynnik rewolucji",
-                        r"Współczynnik siły koloni $\xi$"]) \
-            + endline
-
-    middle = ""
-    for index_params in iterations_results:
-        index, params = index_params
-
-        middle = middle + sep.join([
-            str(index),
-            str(params["evaluation_time"]),
-            str(params["reached_minimum"]),
-            str(params["final_iteration"]),
-            str(params["max_iterations"]),
-            str(params["empires_number"]),
-            str(params["colonies_number"]),
-            str(params["direct_assimilation"]),
-            str(params["avg_colonies_power"]),
-            str(params["revolution_rate"])
-        ]) + endline
-
-    end = r"\end{tabular}"
-    return begin + middle + end
-
-
-iterations_results = []
-
-param_grid = {
+params = {
     "num_of_countries": [100],
     "num_of_imperialist": [5],
-    "max_iterations": [100, 500],
+    "max_iterations": [10, 20, 30, 100, 200],
     "deviation_assimilation": [3.14 / 4.0],
     "direct_assimilation": [0.2],
     "avg_colonies_power": [0.1],
@@ -88,27 +28,10 @@ param_grid = {
     "seed": [420]
 }
 
-grid = ParameterGrid(param_grid)
-function = F1_Elliptic()
-
-for index, params in enumerate(grid):
-    ica = ICA(cost_function=function,
-              num_of_countries=params["num_of_countries"],
-              num_of_imperialist=params["num_of_imperialist"],
-              max_iterations=params["max_iterations"],
-              deviation_assimilation=params["deviation_assimilation"],
-              direct_assimilation=params["direct_assimilation"],
-              avg_colonies_power=params["avg_colonies_power"],
-              revolution_rate=params["revolution_rate"],
-              close_empires_rating=params["close_empires_rating"],
-              seed=params["seed"]
-              )
-    result = ica.eval()
-    metadata = ica.get_evaluation_data()
-    iterations_results.append((index, metadata))
+iterations_results = gridsearch(F1_Elliptic(), params)
 
 print(iterations_results)
 
-file_path = "results"
-create_and_save_plots_to_file(iterations_results, file_path)
-print(create_and_save_params_grid_as_latex_table(iterations_results, file_path))
+file_path = "results/"
+create_and_save_plots_to_file(iterations_results, file_path, "F1")
+print(create_and_save_params_grid_as_latex_table(iterations_results, file_path, "F1"))
---
+++ algorithm_evaluation/evaluation_helper.py
@@ -0,0 +1,79 @@
+import os
+
+from ica.ica import ICA
+from sklearn.model_selection import ParameterGrid
+import matplotlib.pyplot as plt
+
+
+def create_and_save_plots_to_file(iterations_results, file_path, function_name):
+    if not os.path.isdir(file_path):
+        raise RuntimeError()
+    for index_params in iterations_results:
+        index, params = index_params
+        y = params["lowest_cost_per_iteration"]
+        x = range(len(y))
+        plt.plot(x, y, label=str(index))
+    plt.legend()
+    plt.savefig(file_path + function_name + '.png', bbox_inches='tight')
+
+
+def create_and_save_params_grid_as_latex_table(iterations_results, file_path, function_name):
+    sep = " & "
+    endline = r"\\\hline" + "\n"
+
+    begin = r"\begin{tabular}{|c|c|c|c|c|c|c|}" + "\n" \
+            + sep.join(["Nr. wykresu",
+                        "Czas ewaluacji",
+                        "Wartość osiągniętego minimum",
+                        "Ostatnia iteracja",
+                        "Maksymalna liczba iteracji",
+                        "Liczba imperiów",
+                        "Liczba kolonii",
+                        r"Bezpośrednia asymilacja - $\beta$",
+                        "Współczynnik rewolucji",
+                        r"Współczynnik siły koloni $\xi$"]) \
+            + endline
+
+    middle = ""
+    for index_params in iterations_results:
+        index, params = index_params
+
+        middle = middle + sep.join([
+            str(index),
+            str(params["evaluation_time"]),
+            str(params["reached_minimum"]),
+            str(params["final_iteration"]),
+            str(params["max_iterations"]),
+            str(params["empires_number"]),
+            str(params["colonies_number"]),
+            str(params["direct_assimilation"]),
+            str(params["avg_colonies_power"]),
+            str(params["revolution_rate"])
+        ]) + endline
+
+    end = r"\end{tabular}"
+    result = begin + middle + end
+    with open(file_path + function_name + ".txt", "w") as text_file:
+        text_file.write(result)
+    return result
+
+
+def gridsearch(function, params):
+    param_grid = ParameterGrid(params)
+    iterations_results = []
+    for index, params in enumerate(param_grid):
+        ica = ICA(cost_function=function,
+                  num_of_countries=params["num_of_countries"],
+                  num_of_imperialist=params["num_of_imperialist"],
+                  max_iterations=params["max_iterations"],
+                  deviation_assimilation=params["deviation_assimilation"],
+                  direct_assimilation=params["direct_assimilation"],
+                  avg_colonies_power=params["avg_colonies_power"],
+                  revolution_rate=params["revolution_rate"],
+                  close_empires_rating=params["close_empires_rating"],
+                  seed=params["seed"]
+                  )
+        ica.eval()
+        metadata = ica.get_evaluation_data()
+        iterations_results.append((index, metadata))
+    return iterations_results
