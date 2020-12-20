#22 Add rotation matrix, add requirements.txt, fix minor bugs in test functions

algorithm_evaluation/F1/F1_elliptic.py
@@ -18,13 +18,13 @@ class F1_Elliptic(CostFunction):
 tf.config.run_functions_eagerly(True)
 
 params = {
-    "num_of_countries": [500, 1000],
-    "num_of_imperialist": [5, 10],
-    "max_iterations": [4000],
-    "direct_assimilation": [0.2, 0.7],
+    "num_of_countries": [4000, 2000],
+    "num_of_imperialist": [10],
+    "max_iterations": [10000],
+    "direct_assimilation": [0.7, 1.1],
     "avg_colonies_power": [0.1],
-    "revolution_rate": [0.2, 0.5],
-    "seed": [420]
+    "revolution_rate": [0.0, 0.0001],
+    "seed": [None]
 }
 file_path = "algorithm_evaluation/F1/results/"
 iterations_results = gridsearch(F1_Elliptic(), params, file_path)

algorithm_evaluation/F11/F11_schwefel.py
@@ -20,11 +20,8 @@ class F11_Schwefel(CostFunction):
         # 1
         z = matrix - self.o_vector
         # 2
-        result = 0
-        start = 0
-        for i in range(self.s_size):
-            result, start = self.calculate_partial_rotation(i, result, start, z)
-        return result
+        rotated = tf.transpose(self.rotation_matrix.matmul(tf.transpose(matrix)))
+        return self.schwefel_func(rotated)
 
     # TODO remove it - use self.rotation_matrix
     @tf.function
@@ -37,13 +34,13 @@ class F11_Schwefel(CostFunction):
 tf.config.run_functions_eagerly(True)
 
 params = {
-    "num_of_countries": [500, 1000],
-    "num_of_imperialist": [5, 10],
-    "max_iterations": [4000],
-    "direct_assimilation": [0.2, 0.7],
+    "num_of_countries": [4000, 2000],
+    "num_of_imperialist": [10],
+    "max_iterations": [10000],
+    "direct_assimilation": [0.7, 1.1],
     "avg_colonies_power": [0.1],
-    "revolution_rate": [0.2, 0.5],
-    "seed": [420]
+    "revolution_rate": [0.0, 0.0001],
+    "seed": [None]
 }
 
 result_path = "algorithm_evaluation/F11/results/"

algorithm_evaluation/F12/F12_rosenbrock.py
@@ -1,11 +1,12 @@
-import tensorflow as tf
 from algorithm_evaluation.evaluation_helper import *
 from algorithm_evaluation.test_functions import CostFunction
 
 
 class F12_Rosenbrock(CostFunction):
 
-    def __init__(self, upper=100, lower=-100, dimension=1000, o_vector="algorithm_evaluation/F12/resources/F12-xopt.txt"):
+    def __init__(self, upper=100, lower=-100, dimension=1000,
+                 o_vector="algorithm_evaluation/F12/resources/F12-xopt.txt"
+                 ):
         super().__init__(self.rosenbrock, upper, lower, dimension, o_vector)
 
     @tf.function
@@ -17,13 +18,13 @@ class F12_Rosenbrock(CostFunction):
 tf.config.run_functions_eagerly(True)
 
 params = {
-    "num_of_countries": [500, 1000],
-    "num_of_imperialist": [5, 10],
-    "max_iterations": [4000],
-    "direct_assimilation": [0.2, 0.7],
+    "num_of_countries": [4000, 2000],
+    "num_of_imperialist": [10],
+    "max_iterations": [10000],
+    "direct_assimilation": [0.7, 1.1],
     "avg_colonies_power": [0.1],
-    "revolution_rate": [0.2, 0.5],
-    "seed": [420]
+    "revolution_rate": [0.0, 0.0001],
+    "seed": [None]
 }
 
 result_path = "algorithm_evaluation/F12/results/"

algorithm_evaluation/F14/F14_schwefel.py
@@ -25,7 +25,6 @@ class F14_Schwefel(CostFunction):
             result, start = self.calculate_partial_rotation(i, result, start, matrix)
         return result
 
-    # TODO remove it - use self.rotation_matrix
     @tf.function
     def calculate_partial_rotation(self, index, result, start, vector):
         result = result + tf.reduce_sum(
@@ -36,13 +35,13 @@ class F14_Schwefel(CostFunction):
 tf.config.run_functions_eagerly(True)
 
 params = {
-    "num_of_countries": [500, 1000],
-    "num_of_imperialist": [5, 10],
-    "max_iterations": [4000],
-    "direct_assimilation": [0.2, 0.7],
+    "num_of_countries": [4000, 2000],
+    "num_of_imperialist": [10],
+    "max_iterations": [10000],
+    "direct_assimilation": [0.7, 1.1],
     "avg_colonies_power": [0.1],
-    "revolution_rate": [0.2, 0.5],
-    "seed": [420]
+    "revolution_rate": [0.0, 0.0001],
+    "seed": [None]
 }
 
 result_path = "algorithm_evaluation/F14/results/"

algorithm_evaluation/F4/F4_elliptic.py
@@ -20,38 +20,21 @@ class F4_Elliptic(CostFunction):
         # 1
         z = matrix - self.o_vector
         # 2
-        result = 0
-        i = 0
-        start = 0
-        result, i, start = self.calculate_partial_rotation(i, result, start, z)
-        result, i, start = self.calculate_partial_rotation(i, result, start, z)
-        result, i, start = self.calculate_partial_rotation(i, result, start, z)
-        result, i, start = self.calculate_partial_rotation(i, result, start, z)
-        result, i, start = self.calculate_partial_rotation(i, result, start, z)
-        result, i, start = self.calculate_partial_rotation(i, result, start, z)
-        result, i, start = self.calculate_partial_rotation(i, result, start, z)
+        rotated = tf.transpose(self.rotation_matrix.matmul(tf.transpose(matrix)))
         # 3
-        return result + tf.reduce_sum(self.elliptic_func(tf.gather(z, self.p_vector[start:]), start, self.dimension))
-
-    # TODO remove it - use self.rotation_matrix
-    @tf.function
-    def calculate_partial_rotation(self, index, result, start, matrix):
-        result = result + tf.reduce_sum(
-            self.w[index] * self.elliptic_func(self.rotate_vector(matrix, start, self.s[index]), start,
-                                               start + self.s[index]))
-        return result, index + 1, start + self.s[index]
+        return self.elliptic_func(rotated)
 
 
 tf.config.run_functions_eagerly(True)
 
 params = {
-    "num_of_countries": [500, 1000],
-    "num_of_imperialist": [5, 10],
-    "max_iterations": [4000],
-    "direct_assimilation": [0.2, 0.7],
+    "num_of_countries": [2000],
+    "num_of_imperialist": [5],
+    "max_iterations": [5000],
+    "direct_assimilation": [0.2, 0.7, 1.1],
     "avg_colonies_power": [0.1],
     "revolution_rate": [0.2, 0.5],
-    "seed": [420]
+    "seed": [None]
 }
 
 result_path = "algorithm_evaluation/F1/results/"

algorithm_evaluation/F5/F5_rastrigin.py
@@ -20,37 +20,22 @@ class F5_Rastrigin(CostFunction):
         # 1
         z = matrix - self.o_vector
         # 2
-        result = 0
-        i = 0
-        start = 0
-        result, i, start = self.calculate_partial_rotation(i, result, start, z)
-        result, i, start = self.calculate_partial_rotation(i, result, start, z)
-        result, i, start = self.calculate_partial_rotation(i, result, start, z)
-        result, i, start = self.calculate_partial_rotation(i, result, start, z)
-        result, i, start = self.calculate_partial_rotation(i, result, start, z)
-        result, i, start = self.calculate_partial_rotation(i, result, start, z)
-        result, i, start = self.calculate_partial_rotation(i, result, start, z)
-        # 3
-        return result + tf.reduce_sum(self.rastrigin_func(tf.gather(z, self.p_vector[start:])))
+        rotated = tf.transpose(self.rotation_matrix.matmul(tf.transpose(matrix)))
 
-    # TODO remove it - use self.rotation_matrix
-    @tf.function
-    def calculate_partial_rotation(self, index, result, start, vector):
-        result = result + tf.reduce_sum(
-            self.w[index] * self.rastrigin_func(self.rotate_vector(vector, start, self.s[index])))
-        return result, index + 1, start + self.s[index]
+        # 3
+        return self.rastrigin_func(rotated)
 
 
 tf.config.run_functions_eagerly(True)
 
 params = {
-    "num_of_countries": [500, 1000],
-    "num_of_imperialist": [5, 10],
-    "max_iterations": [4000],
-    "direct_assimilation": [0.2, 0.7],
+    "num_of_countries": [4000, 2000],
+    "num_of_imperialist": [10],
+    "max_iterations": [10000],
+    "direct_assimilation": [0.7, 1.1],
     "avg_colonies_power": [0.1],
-    "revolution_rate": [0.2, 0.5],
-    "seed": [420]
+    "revolution_rate": [0.0, 0.0001],
+    "seed": [None]
 }
 
 result_path = "algorithm_evaluation/F5/results/"

algorithm_evaluation/test_functions.py
@@ -10,6 +10,7 @@ class CostFunction:
         self.lower = lower
         self.dimension = dimension
         self.powers = tf.range(0, self.dimension, dtype=tf.float64) / (self.dimension - 1)
+        self.base_powered = tf.math.pow(10e6, self.powers)
         self.s_size = None
         self.overlap = None
 
@@ -25,21 +26,22 @@ class CostFunction:
             self.r_100 = self.read_matrix(r_100, tf.float64)
         if s is not None:
             self.s = self.read_vector(s, tf.int32)
+            self.rotation_matrix = self.create_rotation_matrix()
         if w is not None:
             self.w = self.read_vector(w, tf.float64)
 
     @staticmethod
-    def read_vector(filename, type):
+    def read_vector(filename, elements_type):
         with open(filename, 'r') as file:
             lines = [float(f) for f in file.readlines()]
-            result = tf.cast(tf.constant(lines), type)
+            result = tf.cast(tf.constant(lines), elements_type)
             return result
 
     @staticmethod
-    def read_matrix(filename, type):
+    def read_matrix(filename, elements_type):
         with open(filename, 'r') as file:
             matrix = [[float(num) for num in line.split(',')] for line in file]
-            return tf.cast(tf.constant(matrix), type)
+            return tf.cast(tf.constant(matrix), elements_type)
 
     @tf.function
     def transform_osz(self, x: tf.Tensor):
@@ -79,11 +81,10 @@ class CostFunction:
     def ackley_func(self, matrix: tf.Tensor):
         a = tf.constant(20.0, tf.float64)
         b = tf.constant(0.2, tf.float64)
-        c = tf.constant(constants.two_pi, tf.float64)
         sum1 = tf.reduce_sum(tf.square(matrix), axis=1)
-        sum2 = tf.reduce_sum(tf.math.cos(tf.multiply(c, matrix)), axis=1)
+        sum2 = tf.reduce_sum(tf.math.cos(tf.multiply(constants.two_pi, matrix)), axis=1)
         return -a * tf.exp(-b * tf.sqrt(tf.truediv(sum1, self.dimension))) - \
-               tf.exp(tf.divide(sum2, self.dimension)) + a + constants.e
+               tf.exp(tf.truediv(sum2, self.dimension)) + a + constants.e
 
     @tf.function
     def schwefel_func(self, matrix: tf.Tensor):
@@ -94,17 +95,15 @@ class CostFunction:
         a = tf.constant(100.0, tf.float64)
         x_shift = matrix[:, 1:]
         x_cut = matrix[:, :-1]
-        return tf.reduce_sum(a * tf.square(tf.square(x_cut) - x_shift) - tf.square(x_cut - 1), axis=1)
+        return tf.reduce_sum(a * tf.square(tf.square(x_cut) - x_shift) + tf.square(x_cut - 1), axis=1)
 
     @tf.function
     def elliptic_func(self, matrix: tf.Tensor, start=None, end=None):
-        base = tf.constant(1.0e6, tf.float64)
-        return tf.reduce_sum(tf.math.pow(base, self.powers[start:end, ]) * tf.square(self.transform_osz(matrix)),
-                             axis=1)
+        return tf.reduce_sum(self.base_powered[start:end, ] * tf.square(self.transform_osz(matrix)), axis=1)
 
     @tf.function
     def rotate_vector(self, vector, start, size):
-        rotated = tf.expand_dims(tf.gather(vector, self.p_vector[start:start + size]), 1)
+        rotated = tf.gather(vector, self.p_vector[start:start + size])
         multiplied = tf.case([
             (tf.equal(size, 25), lambda: tf.matmul(self.r_25, rotated)),
             (tf.equal(size, 50), lambda: tf.matmul(self.r_50, rotated)),
@@ -135,3 +134,21 @@ class CostFunction:
             (tf.equal(size, 100), lambda: tf.matmul(self.r_100, rotated)),
         ])
         return tf.squeeze(multiplied)
+
+    @tf.function
+    def create_rotation_matrix(self):
+        operators = []
+        for x in self.s:
+            if x == 25:
+                operators.append(tf.linalg.LinearOperatorFullMatrix(self.r_25))
+            elif x == 50:
+                operators.append(tf.linalg.LinearOperatorFullMatrix(self.r_50))
+            else:
+                operators.append(tf.linalg.LinearOperatorFullMatrix(self.r_100))
+        s_sum = tf.reduce_sum(self.s)
+        if s_sum < self.dimension:
+            difference = self.dimension - s_sum
+            operators.append(tf.linalg.LinearOperatorFullMatrix(tf.eye(difference, difference, dtype=tf.float64)))
+
+        rotation_matrix = tf.linalg.LinearOperatorBlockDiag(operators)
+        return rotation_matrix